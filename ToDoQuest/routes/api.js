715a07c443d5317d0125ca1a4b160aae96324e0a
Jake Chavez <jake94chavez@gmail.com>
2018-03-01 23:38:07 -0600

    starts route for quest addition

--- ToDoQuest/routes/api.js
+++ ToDoQuest/routes/api.js
@@ -2,3 +2,16 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var db = require('../models');
+
+router.post('/quest', (req, res) => {
+  let username = req.session.user.username
+  db.User.find({username: username}).then(function(){
+    let quest = new db.User.Quest(req.body);
+    quest.save((err, createdQuestObject) => {  //.save, saves the info
+      if (err) {
+          res.status(500).send(err);
+      }                                        //numeric codes that tie in with the success and error in ajax
+      res.status(200).send(createdQuestObject);
+    });
+  });
+});
